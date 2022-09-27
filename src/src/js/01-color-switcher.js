3f8ad387c390d3b5ebc6472da8571279093fe0fc
Sergij Suprunenko <104130197+Soteach@users.noreply.github.com>
2022-09-27 15:07:12 +0300

    next step

---
+++ src/src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btn = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector(['[data-stop]']),
+  body: document.querySelector('body'),
+};
+
+btn.stop.disabled = true;
+
+btn.start.addEventListener('click', onStartClick);
+btn.stop.addEventListener('click', onStopClick);
+
+let interval = null;
+
+function onStartClick() {
+  interval = setInterval(() => {
+    btn.body.style.color = getRandomHexColor();
+  }, 1000);
+  btn.start.disabled = true;
+  btn.stop.disabled = false;
+}
+
+function onStopClick() {}
